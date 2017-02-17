42bc89c3efd6377a93d0f020af1fc78ee6a01985
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-02-17 19:55:52 +0530

    about info added

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -2,10 +2,20 @@
     <div class="row">
         <div class="col-md-8">
 			<h3>About the New Catalogus Catalogorum</h3><hr />
-			<p>An Alphabetical Register of Sanskrit Works and Authors</p>
+			<p>
+				Between 1891 and 1903, Simon Theodor Aufrecht (1822 – 1907), a German Sanskritist, published a
+				three volume alphabetical catalogue of all Sanskrit manuscript collections known at the time, in a
+				work titled, Catalogus Catalogorum. This was the first such attempt to catalogue all Indian
+				manuscripts. Beginning in 1935, the University of Madras began working on an updated catalogue
+				called the New Catalogus Catalogorum, and has so far published forty volumes; the project has
+				been funded by various organisations over a long period of time including the National Mission for
+				Manuscripts since its inception. This is an immense resource for, not only Sanskrit scholars but also,
+				all those interested all aspects of Indian Civilisation – Ayurveda, Agriculture, Music, Dance,
+				Mathematics, Sciences, etc.
+			</p>
 		</div>
 		<div class="col-md-4">
-			
+			<img src="<?=PUBLIC_URL?>images/cover.jpg" class="img-thumbnail img-responsive" alt="Cover">
 		</div>
 	</div>
 </div>
